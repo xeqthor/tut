e9ab32d469edab16f05e0e78edfed6db2f21877a
rares olteanu <rares_olteanu@yahoo.com>
2018-08-23 17:18:53 +0300

    changed database and added auth app

--- src/fin2018/settings.py
+++ src/fin2018/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'fin2018.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'fin'),
+        'USER': os.environ.get('DB_USER', 'fin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'fin'),
+        'HOST': os.environ.get('DB_HOST', 'pg'),
     }
 }
 
